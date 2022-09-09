90eb838d87916f34ee3d27ff5d1d22d8c391eb22
Théo RICHARD <theo.richard@omniadefi.com>
2022-09-09 09:51:12 +0100

    feat: create TextInput component

---
+++ src/UI/TextInput.svelte
@@ -0,0 +1,16 @@
+<script>
+  export let controlType;
+  export let id;
+  export let label;
+  export let rows;
+  export let value;
+</script>
+
+<div class="form-control">
+  <label for={id}>{label}</label>
+  {#if controlType === "textaread"}
+    <input type="text" {id} placeholder="set subtitle" {value} on:input />
+  {:else}
+    <textarea {rows} {id} placeholder="set description" {value} on:input />
+  {/if}
+</div>
